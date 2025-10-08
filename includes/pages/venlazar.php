3949c806ec3715a8becc2469fe07ea0564d75aa6
transatlanticvoyage <sandbagexpert@gmail.com>
2025-10-08 13:39:17 -0400

    Initial commit - Saltwater Axiom with Venlazar features

---
+++ includes/pages/venlazar.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Axiom Venlazar Page
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+?>
+
+<div class="wrap">
+    <h1>Venlazar</h1>
+    
+    <div class="axiom-page-content">
+        <!-- Page content will be added here -->
+    </div>
+</div>
